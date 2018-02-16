e8f42ccb900b8e02c718a1bea442fa4e88bd7554
CHRISTOPHER DANIEL HANSON <u1130422@umail.utah.edu>
2018-02-16 11:38:34 -0700

    Add functionality to skeleton methods; add test cases to 90% code coverage

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,283 @@
+﻿// Written by Christopher Hanson for CS 3500, February 2018
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Dependencies;
+using Formulas;
+
+namespace SS
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        /// <summary>
+        /// Dictionary stores association b/w cell names and Cells
+        /// </summary>
+        private Dictionary<string, Cell> sheet;
+        /// <summary>
+        /// DependencyGraph stores relationships b/w Cells
+        /// </summary>
+        private DependencyGraph depGraph;
+
+        /// <summary>
+        /// Constructor; makes new empty spreadsheet
+        /// </summary>
+        public Spreadsheet()
+        {
+            sheet = new Dictionary<string, Cell>();
+            depGraph = new DependencyGraph();
+        }
+
+        /// <summary>
+        /// Enumerates the names of all the non-empty cells in the spreadsheet.
+        /// </summary>
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            List<string> names = new List<string>();
+            foreach (var s in sheet)
+            {
+                if (s.Value.GetContents() != null)
+                {
+                    names.Add(s.Value.GetName());
+                }
+            }
+            return names;
+        }
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, returns the contents (as opposed to the value) of the named cell.  The return
+        /// value should be either a string, a double, or a Formula.
+        /// </summary>
+        public override object GetCellContents(string name)
+        {
+            String validCellNamePattern = @"^[a-zA-Z]*[1-9]\d*$";
+            if (name == null || !Regex.IsMatch(name, validCellNamePattern))
+            {
+                throw new InvalidNameException();
+            }
+            if (sheet.TryGetValue(name, out Cell theCell))
+            {
+                return theCell.GetContents();
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes number.  The method returns a
+        /// set consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            String validCellNamePattern = @"^[a-zA-Z]*[1-9]\d*$";
+            if (name == null || !Regex.IsMatch(name, validCellNamePattern))
+            {
+                throw new InvalidNameException();
+            }
+
+            Cell test = new Cell();
+            if (sheet.TryGetValue(name, out test))
+            {
+                sheet.Remove(name);
+            }
+            Object oldContents = test.GetContents();
+            if (oldContents is Formula) // If old contents are formula, remove relevant dependencies
+            {
+                Formula oldFormula = (Formula)oldContents;
+                foreach (var v in oldFormula.GetVariables())
+                {
+                    depGraph.RemoveDependency(v, name);
+                }
+            }
+                
+            test.SetContents(number);
+            test.SetName(name);
+            sheet.Add(name, test);
+
+            HashSet<string> toReturn = new HashSet<string>();
+            foreach (string s in GetCellsToRecalculate(name))
+            {
+                toReturn.Add(s);
+            }
+            return toReturn;
+        }
+
+        /// <summary>
+        /// If text is null, throws an ArgumentNullException.
+        /// 
+        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes text.  The method returns a
+        /// set consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException("");
+            }
+            String validCellNamePattern = @"^[a-zA-Z]*[1-9]\d*$";
+            if (name == null || !Regex.IsMatch(name, validCellNamePattern))
+            {
+                throw new InvalidNameException();
+            }
+
+            Cell test = new Cell();
+            if (sheet.TryGetValue(name, out test))
+            {
+                sheet.Remove(name);
+            }
+            Object oldContents = test.GetContents();
+            if (oldContents is Formula) // If old contents are formula, remove relevant dependencies
+            {
+                Formula oldFormula = (Formula)oldContents;
+                foreach (var v in oldFormula.GetVariables())
+                {
+                    depGraph.RemoveDependency(v, name);
+                }
+            }
+
+            test.SetContents(text);
+            test.SetName(name);
+            sheet.Add(name, test);
+
+            HashSet<string> toReturn = new HashSet<string>();
+            foreach (string s in GetCellsToRecalculate(name))
+            {
+                toReturn.Add(s);
+            }
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Requires that all of the variables in formula are valid cell names.
+        /// 
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, if changing the contents of the named cell to be the formula would cause a 
+        /// circular dependency, throws a CircularException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes formula.  The method returns a
+        /// Set consisting of name plus the names of all other cells whose value depends,
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            String validCellNamePattern = @"^[a-zA-Z]*[1-9]\d*$";
+            if (name == null || !Regex.IsMatch(name, validCellNamePattern))
+            {
+                throw new InvalidNameException();
+            }
+
+            HashSet<string> vars = (HashSet<string>)formula.GetVariables();
+            foreach (string s in vars) // for each variable in the formula
+            {
+                if (!Regex.IsMatch(s, validCellNamePattern)) // if not valid cell name,
+                {
+                    throw new InvalidNameException(); // throw exception
+                }
+            }
+
+            Cell test = new Cell();
+            if (sheet.TryGetValue(name, out test))
+            {
+                sheet.Remove(name);
+            }
+            Object oldContents = test.GetContents();
+            if (oldContents is Formula) // If old contents are formula, remove relevant dependencies
+            {
+                Formula oldFormula = (Formula)oldContents;
+                foreach (var v in oldFormula.GetVariables())
+                {
+                    depGraph.RemoveDependency(v, name);
+                }
+            }
+            test.SetContents(formula);
+
+            test.SetName(name);
+            sheet.Add(name, test);
+
+            foreach (string s in vars) { // Add new relevant dependencies
+                depGraph.AddDependency(s, name);
+            }
+
+            HashSet<string> toReturn = new HashSet<string>();
+            foreach (string s in GetCellsToRecalculate(name))
+            {
+                toReturn.Add(s);
+            }
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// If name is null, throws an ArgumentNullException.
+        /// 
+        /// Otherwise, if name isn't a valid cell name, throws an InvalidNameException.
+        /// 
+        /// Otherwise, returns an enumeration, without duplicates, of the names of all cells whose
+        /// values depend directly on the value of the named cell.  In other words, returns
+        /// an enumeration, without duplicates, of the names of all cells that contain
+        /// formulas containing name.
+        /// 
+        /// For example, suppose that
+        /// A1 contains 3
+        /// B1 contains the formula A1 * A1
+        /// C1 contains the formula B1 + A1
+        /// D1 contains the formula B1 - C1
+        /// The direct dependents of A1 are B1 and C1
+        /// </summary>
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException("Argument must not be null.");
+            }
+            return depGraph.GetDependents(name);
+        }
+
+        private struct Cell
+        {
+            String name;
+            Object contents;
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            internal void SetName(string n)
+            {
+                name = n;
+            }
+
+            internal object GetContents()
+            {
+                return contents;
+            }
+
+            internal void SetContents(Object theContents)
+            {
+                contents = theContents;
+            }
+        }
+    }
+}
---
+++ Spreadsheet/SpreadsheetTests/BasicSpreadsheetTests.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class BasicSpreadsheetTests
+    {
+        /// <summary>
+        /// Construct new ss, check for fatal errors
+        /// </summary>
+        [TestMethod]
+        public void SSBasicConstructorTest()
+        {
+            Spreadsheet ss = new Spreadsheet();
+        }
+
+        /// <summary>
+        /// Get cell contents on empty cell
+        /// </summary>
+        [TestMethod]
+        public void SSBasicGetOnEmpty()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Object result = ss.GetCellContents("A6");
+            Assert.AreEqual("", result);
+        }
+
+        /// <summary>
+        /// Set cell contents to a double, get contents
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndGetNumber()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", 0.98);
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual(0.98, result);
+        }
+
+        /// <summary>
+        /// Set cell contents to a string, get contents
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndGetString()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", "Cook's");
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual("Cook's", result);
+        }
+
+        /// <summary>
+        /// Set cell contents to a Formula, get contents
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndGetFormula()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("A2-2");
+            ss.SetCellContents("a1", f);
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual(f, result);
+        }
+
+        /// <summary>
+        /// Set cell contents to a Formula with invalid cell name vars, get contents
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SSBasicSetFormulaWithInvalidVars()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("X-2");
+            ss.SetCellContents("a1", f);
+        }
+
+        /// <summary>
+        /// Set cell contents, then set same cell contents again (number); check for result
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndOverrideDouble()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", 0.1);
+            ss.SetCellContents("a1", 0.9);
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual(0.9, result);
+        }
+
+        /// <summary>
+        /// Set cell contents, then set same cell contents again (string); check for result
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndOverrideString()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", "Lake");
+            ss.SetCellContents("a1", "Leelanau");
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual("Leelanau", result);
+        }
+
+        /// <summary>
+        /// Set cell contents, then set same cell contents again (formula); check for result
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndOverrideFormula()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("A1-4");
+            Formula g = new Formula("A2-4");
+            ss.SetCellContents("a1", f);
+            ss.SetCellContents("a1", g);
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual(g, result);
+        }
+
+        /// <summary>
+        /// Set cell contents to formula, then set same cell contents to double; check for result
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndOverrideFormulaWithDouble()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("A1-4");
+            ss.SetCellContents("a1", f);
+            ss.SetCellContents("a1", 5.10);
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual(5.10, result);
+        }
+
+        /// <summary>
+        /// Set cell contents to formula, then set same cell contents to string; check for result
+        /// </summary>
+        [TestMethod]
+        public void SSBasicSetAndOverrideFormulaWithString()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("A1-4");
+            ss.SetCellContents("a1", f);
+            ss.SetCellContents("a1", "Grand Haven");
+            Object result = ss.GetCellContents("a1");
+            Assert.AreEqual("Grand Haven", result);
+        }
+
+        /// <summary>
+        /// Get names of all nonempty, when all are empty
+        /// </summary>
+        [TestMethod]
+        public void SSAllNonEmptyOnAllEmpty()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            IEnumerable<string> result = ss.GetNamesOfAllNonemptyCells();
+            Assert.AreEqual(0, result.Count());
+        }
+
+        /// <summary>
+        /// Get names of all nonempty, with three cells nonempty; test correct number
+        /// </summary>
+        [TestMethod]
+        public void SSGetAllNonEmptyThreeFilledTestNum()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", "apple");
+            ss.SetCellContents("a2", 3.14159);
+            ss.SetCellContents("a3", "<3");
+            IEnumerable<string> result = ss.GetNamesOfAllNonemptyCells();
+            Assert.AreEqual(3, result.Count());
+        }
+
+        /// <summary>
+        /// Get names of all nonempty, with three cells nonempty; test correct names
+        /// </summary>
+        [TestMethod]
+        public void SSGetAllNonEmptyThreeFilledNames()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", "apple");
+            ss.SetCellContents("a2", 3.14159);
+            ss.SetCellContents("a3", "<3");
+            IEnumerable<string> result = ss.GetNamesOfAllNonemptyCells();
+            String[] expected = { "a1", "a2", "a3" };
+            foreach (string s in result)
+            {
+                if (!expected.Contains<string>(s))
+                    Assert.Fail();
+            }
+        }
+
+        /// <summary>
+        /// Get direct dependents of a cell with two
+        /// </summary>
+        [TestMethod]
+        public void SSChangeCellThatIsDependee()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("A1-4");
+            Formula g = new Formula("A1-4");
+            ss.SetCellContents("a1", f);
+            ss.SetCellContents("a2", g);
+            ss.SetCellContents("A1", 0.87);
+            Object result = ss.GetCellContents("A1");
+            Assert.AreEqual(0.87, result);
+        }
+
+        // NULL CHECKS
+        /// <summary>
+        /// Get cell contents with null param
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SSGetCellContentsTestOnNullParam()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.GetCellContents(null);
+        }
+
+        /// <summary>
+        /// Set cell contents (double) with null name param
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SSSetCellContentsTestOnNullParam()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents(null, 0.999);
+        }
+
+        /// <summary>
+        /// Set cell contents to string with null text param
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SSSetCellContentsToStringTestOnNullText()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents("a1", null);
+        }
+
+        /// <summary>
+        /// Set cell contents to string with null name
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SSSetCellContentsToStringTestOnNullName()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            ss.SetCellContents(null, "Unreachable");
+        }
+
+        /// <summary>
+        /// Set cell contents to formula with null name
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void SSSetCellContentsToFormulaTestOnNullName()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Formula f = new Formula("A99+B5");
+            ss.SetCellContents(null, f);
+        }
+    }
+}
