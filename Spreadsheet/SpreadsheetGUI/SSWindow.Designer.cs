3149a6915dce64f5cdaf498deb8b3eb39b6b71d1
Ocmersh <Ocmersh@gmail.com>
2018-03-08 12:04:25 -0700

    .

--- Spreadsheet/SpreadsheetGUI/SSWindow.Designer.cs
+++ Spreadsheet/SpreadsheetGUI/SSWindow.Designer.cs
@@ -175,7 +175,6 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(778, 30);
             this.tableLayoutPanel2.TabIndex = 7;
-            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
             // tableLayoutPanel4
             // 
